c47952d8695d0090741fe5ff1d20771146f2f746
SterbenCyrodiil <rodrigo.coelho1999@gmail.com>
2020-05-19 16:34:34 +0100

    Update: Adicionado método de fitness

--- src/vitor/Cromossoma.java
+++ src/vitor/Cromossoma.java
@@ -5,6 +5,8 @@ import interf.IPoint;
 import interf.IUIConfiguration;
 import maps.Maps;
 
+import java.awt.*;
+import java.awt.geom.Line2D;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Random;
@@ -24,16 +26,24 @@ public class Cromossoma implements Comparable<Cromossoma> {
     }
 
     private int maxTam = 5;
+    private int minTam = 0;
     private int maxMap = 600;
     private int minMap = 0;
 
-    protected int tam = numeroAleatorio(0,maxTam);
-    public List<IPoint> points = new ArrayList<>(); ;
+    private int colisionWeight = 10000;
+
+    private double totaldist;
+
+    protected int tam = numeroAleatorio(minTam,maxTam);
+    public List<IPoint> points = new ArrayList<>();
+    public List<Rectangle> rectangles;
 
     public Cromossoma() {
         points.add(conf.getStart());
         this.starting();
         points.add(conf.getEnd());
+        rectangles = conf.getObstacles();
+        totaldist = 0.0;
     }
 
     public void starting(){
@@ -54,16 +64,74 @@ public class Cromossoma implements Comparable<Cromossoma> {
     }
 
     public void map(){
-
         PathViewer pv = new PathViewer(conf);
-
         pv.setFitness(9999);
         pv.setStringPath("test");
         pv.paintPath(points);
     }
 
+    public void getPoints(){
+        for(int ix=1; ix<this.points.size(); ix++){
+            Point point = (Point) this.points.get(ix);
+            Point prevPoint = (Point) this.points.get(ix-1);
+            System.out.println(prevPoint.getX()+"; "+ point.getX()+"; "+prevPoint.getY()+"; "+ point.getY());
+        }
+    }
+
+    public boolean colisionChecker(){
+
+        boolean colflag = false;
+
+        for(int ix=1; ix<this.points.size(); ix++){
+            Point point = (Point) this.points.get(ix);
+            Point prevPoint = (Point) this.points.get(ix-1);
+            Line2D line2d = new Line2D.Double(prevPoint.getX(), point.getX(), prevPoint.getY(), point.getY());
+            // System.out.println(prevPoint.getX()+"; "+ point.getX()+"; "+prevPoint.getY()+"; "+ point.getY());
+            for(int jx=0; jx<this.rectangles.size(); jx++){
+                boolean collided = line2d.intersects(this.rectangles.get(jx));
+                if(collided){
+                    colflag = true;
+                }
+                //System.out.println("Rectangle: "+jx+"; Touches: "+collided);
+            }
+        }
+        return colflag;
+    }
+
+    public double getDistance(){
+        totaldist = 0.0;
+        for(int ix=1; ix<this.points.size(); ix++){
+            Point point = (Point) this.points.get(ix);
+            Point prevPoint = (Point) this.points.get(ix-1);
+            totaldist = totaldist + Math.sqrt((point.getY() - prevPoint.getY()) * (point.getY() - prevPoint.getY()) + (point.getX() - prevPoint.getX()) * (point.getX() - prevPoint.getX()));
+        }
+        return totaldist;
+    }
+
+    public double getFitness(){
+        double value = 0.0;
+
+        if(colisionChecker()){
+            value = colisionWeight;
+        }
+
+        value = value + getDistance();
+
+        return value;
+    }
+
     @Override
     public int compareTo(Cromossoma o) {
-        return 0;
+        if (o.getFitness() < this.getFitness())
+            return 1;
+        else if (o.getFitness() > this.getFitness())
+            return -1;
+        else return 0;
+    }
+
+    @Override
+    public String toString() {
+        return "Fitness: "+ getFitness()
+                +"Pontos: ";
     }
 }
